6fe20468423372d874fca7cd037908caed0cf4d7
aldo-aguilar <aldoaguilar2022@u.northwestern.edu>
2020-05-24 22:50:52 -0500

    home page done, still early not much here

---
+++ js/sidebar.js
@@ -0,0 +1,8 @@
+var sideBar = document.getElementById('sidebarCollapse')
+
+
+sideBar.onclick = function(){
+    var status = document.getElementById('sidebar')
+    status.classList.toggle('active')
+}
+
