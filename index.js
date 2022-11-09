215a32dcead766ccbb8c2d4e01892a7af454d68e
aldo-aguilar <aldoaguilar2022@u.northwestern.edu>
2022-11-09 11:40:33 -0600

    simple javascript and further cleaning css on projects panel

---
+++ index.js
@@ -0,0 +1,7 @@
+function hover(element) {
+    element.setAttribute('src', 'resources/aldo-in-computer-crossed-eyes.png');
+  }
+  
+  function unhover(element) {
+    element.setAttribute('src', 'resources/aldo-in-computer.png');
+  }
